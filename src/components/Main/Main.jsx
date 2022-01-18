2155692ef67b62778096f9497f636959869869ef
Awais Ahmad <awais.ahmad@mtm.local>
2022-01-18 09:51:20 +0500

    Balance context added

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import {Card,CardHeader,CardContent,Typography,Grid,Divider} from "@material-ui/core";
+import { ExpenseTrackerContext } from '../../context/context';
 
 import Form from "./Form/Form";
 import List from "./List/List";
@@ -8,11 +9,13 @@ import useStyles from "./styles";
 
 const Main = () => {
     const classes = useStyles();
+    const {balance} = useContext(ExpenseTrackerContext);
+
     return (
         <Card className={classes.root}>
             <CardHeader title="Expense Tracker" subheader="Powered By Speechly"/>
             <CardContent>
-                <Typography align="center" variant="h5">Total Balance $150</Typography>
+                <Typography align="center" variant="h5">Total Balance ${balance}</Typography>
                 <Typography variant="subtitle1" style={{lineHeight:"1.5em",marginTop:"20px"}}>
                    
                 </Typography>
