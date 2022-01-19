a348ad0d1ad3f544cb74e3242ef03659e55a9c56
Awais Ahmad <awais.ahmad1990@yahoo.com>
2022-01-19 17:01:31 +0500

    Infocard component added

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,7 @@
 import React,{useContext} from 'react';
 import {Card,CardHeader,CardContent,Typography,Grid,Divider} from "@material-ui/core";
 import { ExpenseTrackerContext } from '../../context/context';
-
+import InfoCard from '../InfoCard';
 import Form from "./Form/Form";
 import List from "./List/List";
 
@@ -17,7 +17,7 @@ const Main = () => {
             <CardContent>
                 <Typography align="center" variant="h5">Total Balance ${balance}</Typography>
                 <Typography variant="subtitle1" style={{lineHeight:"1.5em",marginTop:"20px"}}>
-                   
+                   <InfoCard/>
                 </Typography>
                 <Divider/>
                 <Form/>
